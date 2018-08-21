44aa93f1f1c1947197b8c270fc5392e6f10bd479
egee-irl <brian@egee.io>
2018-08-21 13:22:26 -0700

    the tf script

---
+++ terraform.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+initialize() {
+    printf "Deleting the .terraform directory...\\n"
+    rm -rf .terraform
+
+    printf "Initializing Terraform...\\n"
+    terraform get
+    terraform init
+}
+
+execute() {
+    set -o pipefail
+    terraform $COMMAND | \
+    tee -a "deployment.log"
+}
+
+help() {
+    printf "Available Commands:\\n apply\\n destroy \\n plan\\n\\n"
+}
+
+COMMAND=$1
+AUTO_APPROVE=$3
+
+if [[ -z ${COMMAND} ]]; then
+    help
+    exit 1
+fi
+
+initialize
+execute
